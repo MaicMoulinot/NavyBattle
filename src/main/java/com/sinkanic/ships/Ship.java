6b2e98ca25c9111a266fbb4a50d3fd624c47e59d
Maic <fulldisclosure@hotmail.fr>
2015-05-12 11:49:38 +0200

    Javadoc ships

--- src/main/java/com/sinkanic/ships/Ship.java
+++ src/main/java/com/sinkanic/ships/Ship.java
@@ -17,7 +17,9 @@ public abstract class Ship {
 	private boolean			isVertical;
 
 	/**
-	 * 	Base ship
+	 * @param x int the horizontal position of the first cell
+	 * @param y int the vertical position of the first cell
+	 * @param direction boolean true if the boat is vertical
 	 */
 	public Ship(int size, int gridX, int gridY, boolean vertical) {
 		// Set ship orientation bool
