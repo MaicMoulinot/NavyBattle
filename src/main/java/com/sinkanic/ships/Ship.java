65f2312e64f04174f30a67d61634530c66d0b9d7
unclecreepy <datonton@laposte.net>
2015-05-12 10:59:20 +0200

    added new ship package & started ship inheritance

---
+++ src/main/java/com/sinkanic/ships/Ship.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.sinkanic.ships;
+
+/**
+ * @author matthieumaunier
+ *
+ */
+public class Ship {
+
+	/**
+	 * 
+	 */
+	public Ship() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
