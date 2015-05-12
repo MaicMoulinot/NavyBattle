157941f4001f5547bc3942eca1da6f1754c5c416
similaresolsimi <similaresolsimi@hotmail.com>
2015-05-12 13:49:33 +0200

    guessedCell

--- src/main/java/com/sinkanic/business/PlayerReel.java
+++ src/main/java/com/sinkanic/business/PlayerReel.java
@@ -13,7 +13,7 @@ public class PlayerReel extends Player {
 	
 	public Cell getGuess(int tailleGrilleHorizontal, int tailleGrilleVertical) {
 		Cell guessedCell = new Cell(0, 0);
-		return null;
+		return guessedCell;
 	}
 
 }
