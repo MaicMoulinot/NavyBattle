e67b0912b1f90b4c442619e34eb17c896929519a
unclecreepy <datonton@laposte.net>
2015-05-13 14:10:19 +0200

    added GameController class

---
+++ src/main/java/com/sinkanic/controllers/GameController.java
@@ -0,0 +1,13 @@
+package com.sinkanic.controllers;
+
+import com.sinkanic.models.GameModel;
+
+public class GameController {
+
+	GameModel	model;
+	
+	public GameController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
