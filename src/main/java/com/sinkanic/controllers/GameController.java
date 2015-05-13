51917b6f783e602da594168aec0a76113a6e7c5b
unclecreepy <datonton@laposte.net>
2015-05-13 14:26:42 +0200

    update definitions on Game controller & model.

--- src/main/java/com/sinkanic/controllers/GameController.java
+++ src/main/java/com/sinkanic/controllers/GameController.java
@@ -9,5 +9,14 @@ public class GameController {
 	public GameController() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public boolean addModel(GameModel model) {
+		if (this.model != null) {
+			System.out.println("GameModel.addModel() : already have a bound model.");	
+			return false;
+		}
+		this.model = model;
+		return true;
+	}
 
 }
