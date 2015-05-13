19431fd357095f0a3caa4b068d3dab80042497d7
unclecreepy <datonton@laposte.net>
2015-05-13 15:46:24 +0200

    Moved code from BasicView that shouldn't be there to StartupView.

--- src/main/java/com/sinkanic/views/StartupView.java
+++ src/main/java/com/sinkanic/views/StartupView.java
@@ -1,8 +1,16 @@
 package com.sinkanic.views;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.HeadlessException;
+import java.awt.Insets;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 import com.sinkanic.views.components.BasicView;
+import com.sinkanic.views.components.JButtonDemarrer;
+import com.sinkanic.views.components.QuitButton;
 
 public class StartupView extends BasicView {
 
@@ -16,8 +24,42 @@ public class StartupView extends BasicView {
 	}
 
 	public StartupView(String title) throws HeadlessException {
-		super(title);
-		// TODO Auto-generated constructor stub
+		super(title);		
+		initUI();
+	}
+	private void initUI() {
+		setLayout(new GridBagLayout());
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.insets = new Insets(5, 5, 5, 5);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.gridwidth = 2;
+		add(new JLabel("Welcome bro !"), gbc);
+
+		gbc.gridwidth = 1;
+		JLabel lblName = new JLabel("Name:");
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		add(lblName, gbc);
+		
+
+		JTextField tfInputName = new JTextField();
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+		add(tfInputName, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+		add(new JButtonDemarrer(), gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 3;
+		add(new QuitButton(), gbc);
+		
+		validate();
 	}
 
 }
--- src/main/java/com/sinkanic/views/components/BasicView.java
+++ src/main/java/com/sinkanic/views/components/BasicView.java
@@ -1,15 +1,8 @@
 package com.sinkanic.views.components;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.HeadlessException;
-import java.awt.Insets;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 public abstract class BasicView extends JFrame {
 
@@ -21,7 +14,6 @@ public abstract class BasicView extends JFrame {
 	
 	public BasicView(String title) throws HeadlessException {
 		setViewProperties(title, 300, 200);
-		initUI();
 	}
 
 	private void setViewProperties(String title, int width, int height) {
@@ -30,40 +22,7 @@ public abstract class BasicView extends JFrame {
 		setVisible(true);
 		setResizable(false);
 		setLocationRelativeTo(null);
-		setLayout(new GridBagLayout());
 	}
 	
-	private void initUI() {
-		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		gbc.insets = new Insets(5, 5, 5, 5);
-		
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.gridwidth = 2;
-		add(new JLabel("Welcome bro !"), gbc);
 
-		gbc.gridwidth = 1;
-		JLabel lblName = new JLabel("Name:");
-		gbc.gridx = 0;
-		gbc.gridy = 1;
-		add(lblName, gbc);
-		
-
-		JTextField tfInputName = new JTextField();
-		gbc.gridx = 1;
-		gbc.gridy = 1;
-		add(tfInputName, gbc);
-		
-		gbc.gridx = 0;
-		gbc.gridy = 3;
-		add(new JButtonDemarrer(), gbc);
-		
-		gbc.gridx = 1;
-		gbc.gridy = 3;
-		add(new QuitButton(), gbc);
-		
-		//validate();
-		//pack();
-	}
 }
