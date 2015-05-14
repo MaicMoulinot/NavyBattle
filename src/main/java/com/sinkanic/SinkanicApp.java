36d6851f1868e655feb11ff7854b7261530d59fb
unclecreepy <datonton@laposte.net>
2015-05-14 15:47:46 +0200

    minor changes to SinkanicApp.java

--- src/main/java/com/sinkanic/SinkanicApp.java
+++ src/main/java/com/sinkanic/SinkanicApp.java
@@ -1,17 +1,12 @@
 package com.sinkanic;
 
 import com.sinkanic.controllers.LayShipsController;
-import com.sinkanic.controllers.StartupController;
 import com.sinkanic.models.GameModel;
 import com.sinkanic.views.LayShipsView;
-import com.sinkanic.views.StartupView;
-import com.sinkanic.views.components.SView;
 
 public class SinkanicApp {
 
 	private GameModel			gameModel;
-	private StartupController	startController;
-	private SView				startView;
 
 	public SinkanicApp() {
 		gameModel = new GameModel();
@@ -20,7 +15,7 @@ public class SinkanicApp {
 	
 	private void run() {
 		gameModel = new GameModel();
-		startController = new StartupController();
+		//startController = new StartupController();
 		//startView = new StartupView(startController);
 		
 		//startController.addModel(gameModel);
